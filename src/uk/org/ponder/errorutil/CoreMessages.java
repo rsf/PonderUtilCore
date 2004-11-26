a349810f3c8566ed84ef4936a17b57c4b7394cd5
Antranig Basman <amb26@ponder.org.uk>
2004-11-26 23:48:10 +0000

    Weekend checkin

---
+++ src/uk/org/ponder/errorutil/CoreMessages.java
@@ -0,0 +1,12 @@
+/*
+ * Created on Nov 23, 2004
+ */
+package uk.ac.cam.caret.usermessages;
+
+/**
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ * 
+ */
+public class CoreMessages {
+  public static String LOGON_AUTH_FAILED = "AuthFailed";
+}
