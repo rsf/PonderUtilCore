ecd694b5d73eb5ddfb8146d9abfdc2d5c4399f85
Antranig Basman <amb26@ponder.org.uk>
2005-07-08 21:45:53 +0000

    TimeUtil

---
+++ src/uk/org/ponder/timeutil/CalendarFactory.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Jul 8, 2005
+ */
+package uk.org.ponder.timeutil;
+
+import java.util.Calendar;
+
+/**
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ * 
+ */
+public interface CalendarFactory {
+  public Calendar getCalendar();
+}
