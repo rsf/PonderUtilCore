82de0756bb7ceb9106e6a5ce6d66df6658df4768
Antranig Basman <amb26@ponder.org.uk>
2005-08-04 14:54:10 +0000

    Minor CharWrap change, mid-work on RSF

---
+++ src/uk/org/ponder/beanutil/BeanGetter.java
@@ -0,0 +1,12 @@
+/*
+ * Created on Mar 14, 2005
+ */
+package uk.org.ponder.webapputil;
+
+/**
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ * 
+ */
+public interface BeanGetter {
+  public Object getBean(String beanname);
+}
