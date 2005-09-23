e595e111cd005346ccbd211b7d9934862bb8e5e7
nobody <nobody@localhost>
2005-09-23 02:02:02 +0000

    This commit was manufactured by cvs2svn to create tag 'FLOWTALK_SAKAI_2_0'.

---
+++ src/uk/org/ponder/streamutil/StringPOS.java
@@ -0,0 +1,42 @@
+/*
+ * Created on Sep 15, 2005
+ */
+package uk.org.ponder.streamutil;
+
+import uk.org.ponder.stringutil.CharWrap;
+
+public class StringPOS implements PrintOutputStream {
+  private CharWrap buffer = new CharWrap();
+
+  public void println(String toprint) {
+    print(toprint);
+    print("\n");
+  }
+
+  public void flush() {
+  }
+
+  public void close() {
+  }
+
+  public PrintOutputStream print(String string) {
+    buffer.append(string);
+    return this;
+  }
+
+  public void println() {
+    println("");
+  }
+
+  public void println(Object obj) {
+    println(obj.toString());
+  }
+
+  public String toString() {
+    return buffer.toString();
+  }
+
+  public void write(char[] storage, int offset, int size) {
+   buffer.append(storage, offset, size);
+  }
+}
