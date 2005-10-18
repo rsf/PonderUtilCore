a596475c9fb49fa15f800fad6755c7a96732606e
Raymond Chan <raymond@caret.cam.ac.uk>
2005-10-18 18:55:25 +0000

    put the /real/ head back in

--- src/uk/org/ponder/beanutil/BeanContainerWrapper.java
+++ src/uk/org/ponder/beanutil/BeanContainerWrapper.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import uk.org.ponder.arrayutil.ArrayUtil;
 import uk.org.ponder.saxalizer.SAXAccessMethod;
 import uk.org.ponder.stringutil.StringList;
+import uk.org.ponder.util.ReflectiveCache;
 import uk.org.ponder.util.UniversalRuntimeException;
 
 /**
@@ -16,11 +17,8 @@ import uk.org.ponder.util.UniversalRuntimeException;
  * 
  */
 public class BeanContainerWrapper implements RootBeanLocator {
-
   private BeanGetter factory;
   private String[] permittedroots;
-  
-  private HashMap cachedmethods = new HashMap();
   
   public void setBeanGetter(BeanGetter beangetter) {
     factory = beangetter;
@@ -50,28 +48,19 @@ public class BeanContainerWrapper implements RootBeanLocator {
     return bean;
   }
   
-  public void invokeBeanMethod(String pathwithmethod) {
-    Method method = (Method) cachedmethods.get(pathwithmethod);
+  public Object invokeBeanMethod(String pathwithmethod) {
     String totail = PathUtil.getToTailPath(pathwithmethod);
+    String methodname = PathUtil.getTailPath(pathwithmethod);
     Object bean = getBean(totail);
-     
-    if (method == null) {
-      String methodname = PathUtil.getTailPath(pathwithmethod);
-      try {
-        method = bean.getClass().getMethod(methodname, SAXAccessMethod.emptyclazz);
-      }
-      catch (Exception e) {
-        throw UniversalRuntimeException.accumulate(e, "Error reflecting for method " + methodname + " in bean of " + 
-            bean.getClass() + " at path " + totail);
-      }
-      cachedmethods.put(pathwithmethod, method);
-    }
     try {
-      method.invoke(bean, SAXAccessMethod.emptyobj);
+      
+      return ReflectiveCache.invokeMethod(bean, methodname);
     }
     catch (Exception e) {
-      throw UniversalRuntimeException.accumulate(e, "Error invoking method for path " + pathwithmethod);
-    }
+      throw UniversalRuntimeException.accumulate(e, "Error invoking method " + methodname + " in bean of " + 
+          bean.getClass() + " at path " + totail);
+      }
+    
   }
  
 
