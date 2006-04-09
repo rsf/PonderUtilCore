522d762fbc43a2c9d17ce5c4c0a2d2aa60f9fa54
Antranig Basman <amb26@ponder.org.uk>
2006-04-09 02:21:22 +0000

    MIP comment

--- src/uk/org/ponder/reflect/MethodInvokingProxy.java
+++ src/uk/org/ponder/reflect/MethodInvokingProxy.java
@@ -3,6 +3,13 @@
  */
 package uk.org.ponder.reflect;
 
+/** When encountered in a bean model at path where a method invocation is being
+ * attempted via EL, will accept the invocation via this method call rather than
+ * having it applied reflectively.
+ * @author Antranig Basman (amb26@ponder.org.uk)
+ *
+ */
+
 public interface MethodInvokingProxy {
   public Object invokeMethod(String name, Object[] args);
 }
