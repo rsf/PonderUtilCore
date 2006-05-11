c4243f982f1e55c34cafbbe94fea3a2e995a9c44
Antranig Basman <amb26@ponder.org.uk>
2006-05-11 23:55:44 +0000

    Removal of invalid Unicode declaration for IBM compiler workaround

--- src/uk/org/ponder/util/Constants.java
+++ src/uk/org/ponder/util/Constants.java
@@ -7,6 +7,6 @@ public interface Constants {
    * where an identifiable non-null key is required. 
    * Never try to store or transmit this character outside the JVM!
    */
-  public static final String UNUSED_STRING = "\ufdd0";
+  //public static final String UNUSED_STRING = "\ ufdd0";
   public static final double root2 = Math.sqrt(2);
   }
