efb635a235dbaaa7584389fe456061e674d001b5
Antranig Basman <amb26@ponder.org.uk>
2007-09-03 21:48:49 +0000

    New methods and comments for MapUtil - make

--- src/uk/org/ponder/arrayutil/MapUtil.java
+++ src/uk/org/ponder/arrayutil/MapUtil.java
@@ -4,8 +4,11 @@
 package uk.org.ponder.arrayutil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+/** Utilities for working with java.util.Map instances **/
 
 public class MapUtil {
   /** I have finally had *enough* of writing this code! */
@@ -16,5 +19,20 @@ public class MapUtil {
       target.put(key, oldlist);
     }
     oldlist.add(value);
+  }
+  
+  /** Construct a Map consisting of the single supplied key/value pair */
+  public static Map make(Object key, Object value) {
+    Map togo = new HashMap();
+    togo.put(key, value);
+    return togo;
+  }
+  
+  /** Construct a Map holding the two supplied key/value pairs */
+  public static Map make(Object key1, Object value1, Object key2, Object value2) {
+    Map togo = new HashMap();
+    togo.put(key1, value1);
+    togo.put(key2, value2);
+    return togo;
   }
 }
