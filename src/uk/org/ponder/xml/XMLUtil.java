1275a24f0eb90a6811774707f52aa08c25d6dc86
Antranig Basman <amb26@ponder.org.uk>
2007-11-06 16:32:00 +0000

    XMLUtil flattening method + test

--- src/uk/org/ponder/xml/XMLUtil.java
+++ src/uk/org/ponder/xml/XMLUtil.java
@@ -7,6 +7,9 @@ import java.util.Iterator;
 import java.util.Map;
 
 import uk.org.ponder.streamutil.write.StringPOS;
+import uk.org.ponder.stringutil.ByteToCharBase64;
+import uk.org.ponder.stringutil.CharToByteBase64;
+import uk.org.ponder.stringutil.CharWrap;
 
 public class XMLUtil {
 
@@ -30,6 +33,28 @@ public class XMLUtil {
     XMLWriter xmlw = new XMLWriter(pos);
     xmlw.write(toencode);
     return pos.toString();
+  }
+  
+  /** Flatten a id (assumed generated via the EighteenIDGenerator) into
+   * one that may form a valid XML ID.
+   * http://www.w3.org/TR/REC-xml/#NT-Name
+   */
+  public static String produceXMLID(String id) {
+    int oldi = CharToByteBase64.pem_reverse_array[id.charAt(0)];
+    if (oldi == -1) {
+      throw new IllegalArgumentException("First character of id " + id + " not recognized");
+    }
+    CharWrap togo = new CharWrap(24);
+    togo.append(ByteToCharBase64.pem_array[oldi % 52]);
+    for (int i = 1; i < id.length(); ++ i) {
+      char c = id.charAt(i);
+      togo.append(c == '('? '.' : c);
+    }
+    return togo.toString();
+  }
+  
+  public static final boolean isXMLNameChar(char c) {
+    return Character.isLetter(c) || Character.isDigit(c) || c == '.' || c == '-' || c == '_' || c == ':';
   }
   
 }
---
+++ test/uk/org/ponder/test/dateutil/TestXMLUtil.java
@@ -0,0 +1,24 @@
+/*
+ * Created on 6 Nov 2007
+ */
+package uk.org.ponder.test.dateutil;
+
+import uk.org.ponder.hashutil.EighteenIDGenerator;
+import uk.org.ponder.hashutil.IDGenerator;
+import uk.org.ponder.xml.XMLUtil;
+import junit.framework.TestCase;
+
+public class TestXMLUtil extends TestCase {
+  public void testFlatten() {
+    IDGenerator idGenerator = new EighteenIDGenerator();
+    for (int i = 0; i < 100; ++ i) {
+      String original = idGenerator.generateID();
+      String flat = XMLUtil.produceXMLID(original);
+      System.out.println(original + " " + flat);
+      assertTrue(Character.isLetter(flat.charAt(0)));
+      for (int j = 1; j < flat.length(); ++ j) {
+       assertTrue(XMLUtil.isXMLNameChar(flat.charAt(j)));
+      }
+    }
+  }
+}
