befaf5d8fd2d5321609c4ba137ab70f3b74ec509
David Horwitz <dhorwitz@gmail.com>
2011-07-19 15:32:57 +0200

    Extend Exception rather than Throwable. Closes #4

--- src/uk/org/ponder/errorutil/ConfigurationException.java
+++ src/uk/org/ponder/errorutil/ConfigurationException.java
@@ -7,6 +7,11 @@ package uk.org.ponder.errorutil;
  * @author Antranig Basman (antranig@caret.cam.ac.uk)
  * 
  */
-public class ConfigurationException extends Throwable {
+public class ConfigurationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7299672520889194087L;
 
 }
